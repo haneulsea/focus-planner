bd8fce4f42f1f0d391dc0c63f30a7b4300e9221c
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-03 02:19:14 -0700

    Initialize project with base files

---
+++ src/main/java/io/github/haneulsea/focusplanner/FocusPlannerApplication.java
@@ -0,0 +1,13 @@
+package io.github.haneulsea.focusplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/haneulsea/focusplanner/FocusPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.haneulsea.focusplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
