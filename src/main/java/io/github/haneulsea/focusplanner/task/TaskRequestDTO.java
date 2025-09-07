9032e85e033a0b4c7178e2c0459adcb9a854090b
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-06 17:49:13 -0700

    Add TaskRequestDTO with base fields

---
+++ src/main/java/io/github/haneulsea/focusplanner/task/TaskRequestDTO.java
@@ -0,0 +1,23 @@
+package io.github.haneulsea.focusplanner.task;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+public class TaskRequestDTO {
+
+    @NotBlank(message = "Title must not be blank")
+    private String title;
+
+    @NotNull(message = "Start time is required")
+    private LocalTime startTime;
+
+    @NotNull(message = "End time is required")
+    private LocalTime endTime;
+
+}
