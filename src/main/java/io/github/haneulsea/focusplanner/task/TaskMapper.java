783227579444ef4d88d4a6675d1abb06c9e06b8e
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-08 22:59:25 -0700

    Add updateEntity method

--- src/main/java/io/github/haneulsea/focusplanner/task/TaskMapper.java
+++ src/main/java/io/github/haneulsea/focusplanner/task/TaskMapper.java
@@ -1,6 +1,7 @@
 package io.github.haneulsea.focusplanner.task;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
@@ -8,5 +9,7 @@ public interface TaskMapper {
     Task toEntity(TaskRequest dto);
 
     TaskResponse toResponse(Task entity);
+
+    public void updateEntity(TaskRequest dto, @MappingTarget Task entity);
 
 }
