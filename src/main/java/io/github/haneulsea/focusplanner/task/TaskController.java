6f023d6a09577e6375384a24a5d81d09c878f56b
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-11 23:42:37 -0700

    Add getAllTasks handler method

--- src/main/java/io/github/haneulsea/focusplanner/task/TaskController.java
+++ src/main/java/io/github/haneulsea/focusplanner/task/TaskController.java
@@ -1,10 +1,24 @@
 package io.github.haneulsea.focusplanner.task;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
-    
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<TaskResponse> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
 }
