a8627e258d918dfd7432ee421112cb2afa641d50
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-16 16:58:13 -0700

    Update task entity with getters and setters

--- src/main/java/io/github/haneulsea/focusplanner/task/Task.java
+++ src/main/java/io/github/haneulsea/focusplanner/task/Task.java
@@ -1,6 +1,7 @@
 package io.github.haneulsea.focusplanner.task;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalTime;
 
@@ -11,13 +12,47 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotBlank(message = "Title is required")
     @Column(nullable = false)
     private String title;
 
+    @NotBlank(message = "Time is required")
     @Column(nullable = false)
     private LocalTime startTime;
 
+    @NotBlank(message = "Time is required")
     @Column(nullable = false)
     private LocalTime endTime;
+
+    public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 
 }
