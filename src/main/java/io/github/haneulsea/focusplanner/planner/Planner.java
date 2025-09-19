c9db7bc00754119be301b507d8233b8744ec2416
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-18 22:21:32 -0700

    Add id, title fields and constructor

--- src/main/java/io/github/haneulsea/focusplanner/planner/Planner.java
+++ src/main/java/io/github/haneulsea/focusplanner/planner/Planner.java
@@ -1,5 +1,20 @@
 package io.github.haneulsea.focusplanner.planner;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Planner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Title is required")
+    @Column(nullable = false)
+    private String title;
+
+    public Planner() {
+    }
 
 }
