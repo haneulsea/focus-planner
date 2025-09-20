fa1a1a4311b7d49274d423fb0bed4b5650f38f22
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-19 23:53:25 -0700

    Map one planner entity to many task entities

--- src/main/java/io/github/haneulsea/focusplanner/planner/Planner.java
+++ src/main/java/io/github/haneulsea/focusplanner/planner/Planner.java
@@ -1,7 +1,11 @@
 package io.github.haneulsea.focusplanner.planner;
 
+import io.github.haneulsea.focusplanner.task.Task;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Planner {
@@ -13,6 +17,9 @@ public class Planner {
     @NotBlank(message = "Title is required")
     @Column(nullable = false)
     private String title;
+
+    @OneToMany(mappedBy = "planner", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
 
     public Planner() {
     }
