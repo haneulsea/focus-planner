29b047143c5d5fde233203a8bea6eeed6eb752a6
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-09-25 21:30:52 -0700

    Add base CRUD handler methods for Planner

--- src/main/java/io/github/haneulsea/focusplanner/planner/PlannerController.java
+++ src/main/java/io/github/haneulsea/focusplanner/planner/PlannerController.java
@@ -1,5 +1,44 @@
 package io.github.haneulsea.focusplanner.planner;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/planners")
 public class PlannerController {
+
+    private final PlannerService plannerService;
+
+    public PlannerController(PlannerService plannerService) {
+        this.plannerService = plannerService;
+    }
+
+    @PostMapping
+    public Planner createPlanner(@RequestBody Planner planner) {
+        return plannerService.createPlanner(planner);
+    }
+
+    @GetMapping
+    public List<Planner> getAllPlanners() {
+        return plannerService.getAllPlanners();
+    }
+
+    @GetMapping("/{plannerId}")
+    public Planner getPlannerById(@PathVariable Integer plannerId) {
+        return plannerService.getPlannerById(plannerId);
+    }
+
+    @PatchMapping("/{plannerId}")
+    public Planner updatePlannerById(@PathVariable Integer plannerId,
+                                     @RequestBody Map<String, Object> updates) {
+        return plannerService.updatePlannerById(plannerId, updates);
+    }
+
+    @DeleteMapping("/{plannerId}")
+    public void deletePlannerById(@PathVariable Integer plannerId) {
+        plannerService.deletePlannerById(plannerId);
+    }
 
 }
